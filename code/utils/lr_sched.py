d4d9fa1412ce1e5be9e22b0789878e759b91a1a5
Eren <erenkx@icloud.com>
2025-08-29 19:32:36 +1000

    Add lr_sched.py for cosine learning rate scheduler with warmup

---
+++ code/utils/lr_sched.py
@@ -0,0 +1,31 @@
+"""
+Learning rate scheduler with cosine decay and warmup.
+
+Originally adapted from the MAE codebase:
+https://github.com/facebookresearch/mae
+"""
+
+import math
+
+
+def adjust_learning_rate(optimizer, epoch, args):
+    """
+    Applies cosine decay learning rate with linear warmup.
+    """
+    if epoch < args.warmup_epochs:
+        lr = args.lr * epoch / args.warmup_epochs
+    else:
+        lr = args.lr + (args.lr - args.min_lr) * 0.5 * (
+            1.0 + math.cos(
+                math.pi * (epoch - args.warmup_epochs)
+                / (args.max_communication_rounds - args.warmup_epochs)
+            )
+        )
+
+    for param_group in optimizer.param_groups:
+        if 'lr_scale' in param_group:
+            param_group['lr'] = lr * param_group['lr_scale']
+        else:
+            param_group['lr'] = lr
+
+    return lr
